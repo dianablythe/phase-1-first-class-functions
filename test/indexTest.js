5de12efbcae8cf29b83173966711ef100169de9d
Diana Gelinas <dianablythegelinas@gmail.com>
2022-05-18 11:37:19 -0600

    commit tree 3a37776ce71a4e7e1da09719b5d0b161b8d15dff

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,47 +1,47 @@
-const chai = require("chai");
-const spies = require("chai-spies");
-chai.use(spies);
+// //const chai = require("chai");
+// const spies = require("chai-spies");
+// chai.use(spies);
 
-describe("index", () => {
-  describe("receivesAFunction(callback)", () => {
-    it("receives a function and calls it", () => {
-      const spy = chai.spy();
+// describe("index", () => {
+//   describe("receivesAFunction(callback)", () => {
+//     it("receives a function and calls it", () => {
+//       const spy = chai.spy();
 
-      receivesAFunction(spy);
+//       receivesAFunction(spy);
 
-      expect(spy).to.have.been.called();
-    });
-  });
+//       expect(spy).to.have.been.called();
+//     });
+//   });
 
-  describe("returnsANamedFunction()", () => {
-    var fn;
+//   describe("returnsANamedFunction()", () => {
+//     var fn;
 
-    before(() => {
-      fn = returnsANamedFunction();
-    });
+//     before(() => {
+//       fn = returnsANamedFunction();
+//     });
 
-    it("returns a function", () => {
-      expect(fn).to.be.a("function");
-    });
+//     it("returns a function", () => {
+//       expect(fn).to.be.a("function");
+//     });
 
-    it("returns a named function", () => {
-      expect(fn.name).not.to.eql("");
-    });
-  });
+//     it("returns a named function", () => {
+//       expect(fn.name).not.to.eql("");
+//     });
+//   });
 
-  describe("returnsAnAnonymousFunction()", () => {
-    var fn;
+//   describe("returnsAnAnonymousFunction()", () => {
+//     var fn;
 
-    before(() => {
-      fn = returnsAnAnonymousFunction();
-    });
+//     before(() => {
+//       fn = returnsAnAnonymousFunction();
+//     });
 
-    it("returns a function", () => {
-      expect(fn).to.be.a("function");
-    });
+//     it("returns a function", () => {
+//       expect(fn).to.be.a("function");
+//     });
 
-    it("returns an anonymous function", () => {
-      expect(fn.name).to.eql("");
-    });
-  });
-});
+//     it("returns an anonymous function", () => {
+//       expect(fn.name).to.eql("");
+//     });
+//   });
+// });
